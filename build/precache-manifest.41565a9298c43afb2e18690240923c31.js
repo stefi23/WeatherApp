c97555f13c898f257d3404f49054c82b8bf6f373
stefi23 <stefania.rosca2309@gmail.com>
2020-01-06 17:25:04 +0100

    First commit

---
+++ build/precache-manifest.41565a9298c43afb2e18690240923c31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922ff376ac2eee22c03305aee6be9562",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "e2451821ba0c9bbd4556",
+    "url": "/WeatherApp/static/css/main.0e041fa6.chunk.css"
+  },
+  {
+    "revision": "a4eb48e4087061c79f78",
+    "url": "/WeatherApp/static/js/2.03bc5cfd.chunk.js"
+  },
+  {
+    "revision": "e2451821ba0c9bbd4556",
+    "url": "/WeatherApp/static/js/main.203ae874.chunk.js"
+  },
+  {
+    "revision": "1d5ad4213c032329a4d6",
+    "url": "/WeatherApp/static/js/runtime-main.b8f62223.js"
+  }
+]);
