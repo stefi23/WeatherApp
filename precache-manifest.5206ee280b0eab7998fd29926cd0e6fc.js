6a5ae34dccfaa5cb7e67e767617404fa5e2a2466
stefi23 <stefania.rosca2309@gmail.com>
2020-01-06 23:34:00 +0100

    Updates

---
+++ precache-manifest.5206ee280b0eab7998fd29926cd0e6fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3da246df6400c0d98f187c2af4400a",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "3c8f7e0bec08dd2e650d",
+    "url": "/WeatherApp/static/css/main.0e041fa6.chunk.css"
+  },
+  {
+    "revision": "89bd96676d5380cfeb1c",
+    "url": "/WeatherApp/static/js/2.1a90a100.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/WeatherApp/static/js/2.1a90a100.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c8f7e0bec08dd2e650d",
+    "url": "/WeatherApp/static/js/main.a1e0e548.chunk.js"
+  },
+  {
+    "revision": "bea3f95eb4b1d3ce26a7",
+    "url": "/WeatherApp/static/js/runtime-main.93f198d9.js"
+  }
+]);
